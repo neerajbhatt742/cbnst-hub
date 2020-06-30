ad1e8a1e4149b7c2c35aff734e2cbc30d59527bd
neeraj bhatt <neerajbhatt742@gmail.com>
2020-06-30 21:26:14 +0530

    init

---
+++ function.js
@@ -0,0 +1,12 @@
+function f(x)
+        {
+            //var a=prompt("enter");
+            var z= document.getElementById("F1").value;
+            //var x=a;
+            with(Math)
+            {
+                var ans=eval(z);
+            }
+            //alert(ans);
+            return ans;
+        }
---
+++ javascript.js
@@ -0,0 +1,2 @@
+const {derivative}=require( 'mathjs')
+console.log(math.derivative('2x^2 + 3x + 4', 'x').toString())
